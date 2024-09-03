dd6f7927435323a6af7d71c944338be7a9cd87ae
RAJEEV RANJAN <106504730+rajeevranjan20@users.noreply.github.com>
2024-09-03 11:32:47 +0530

    Time: 41 ms (14.91%), Space: 16.5 MB (17.99%) - LeetHub

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        res=0
+        curr=0
+        for c in s:
+            if c=='(':
+                curr+=1
+            elif c==')':
+                curr-=1
+            res=max(res,curr)    
+        return res    
